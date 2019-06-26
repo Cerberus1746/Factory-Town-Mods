91563da1a46a7ea9c8bbde3a557461d441eac7b0
Leandro Benedet Garcia <cerberus1746@gmail.com>
2019-06-26 12:51:04 -0300

    Created branch for MonoMod

--- ModdingApi/Main.cs
+++ ModdingApi/Main.cs
@@ -1,42 +1,11 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
+﻿using BepInEx;
 
 namespace ModdingApi {
-    class Loader {
-        public static void Main() {
-            try {
-                Assembly mod = Assembly.LoadFile("D:\\Program Files (x86)\\Steam\\steamapps\\common\\Factory Town\\Mods\\huge_silo\\HugeSilo.dll");
-                AppDomain.CurrentDomain.Load(mod.GetName());
-                Type mainType = mod.GetType("HugeSilo.Main");
-
-                if(mainType != null) {
-                    MethodInfo loadMethod = mainType.GetMethod("Load");
-                    if(loadMethod != null) {
-                        loadMethod.Invoke(null, null);
-                    } else {
-                        throw new FileLoadException("Cannot find Load Method.");
-                    }
-                } else {
-                    throw new FileLoadException("Cannot find Main class.");
-                }
-
-        } catch(Exception ex){
-                using(StreamWriter writer = new StreamWriter("modding_logs\\modding_error_file.txt")) {
-                    writer.WriteLine("-----------------------------------------------------------------------------");
-                    writer.WriteLine("Date : " + DateTime.Now.ToString());
-                    writer.WriteLine();
-
-                    while(ex != null) {
-                        writer.WriteLine(ex.GetType().FullName);
-                        writer.WriteLine("Message : " + ex.Message);
-                        writer.WriteLine("StackTrace : " + ex.StackTrace);
-
-                        ex = ex.InnerException;
-                    }
-                }
-                throw;
-            }
+    [BepInPlugin("org.bepinex.plugins.exampleplugin", "Example Plug-In", "1.0.0.0")]
+    public class ExamplePlugin : BaseUnityPlugin {
+        // Awake is called once when both the game and the plug-in are loaded
+        void Awake() {
+            UnityEngine.Debug.Log("Hello, world!");
         }
     }
 }
